ea1ebc915a0d746aea1cf0bb6097899a0b522eab
Luciano Strika <lucianostrika44@gmail.com>
2017-12-21 22:56:50 -0300

    newton fractals are now generated asynchronously, line by line, giving the code a huge boost in performance.

--- newtonFractals.go
+++ newtonFractals.go
@@ -11,38 +11,44 @@ import (
 func main() {
 	const (
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
-		width, height = 1024, 1024
+		width, height = 4096, 4096
 	)
-	clarity,_:=strconv.ParseInt(os.Args[1], 10,8)
+	contrast,_:=strconv.ParseInt(os.Args[1], 10,8)
 	t,_:= strconv.ParseInt(os.Args[2], 10,8)
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
-		y := float64(py)/height*(ymax-ymin) + ymin
-		for px := 0; px < width; px++ {
-		    x := float64(px)/width*(xmax-xmin) + xmin
-	            z := complex(x, y)
-		    img.Set(px, py, newton(z,uint8(clarity),int(t)))
-		}
+		go setLine(ymin,ymax,xmin,xmax,width,height,contrast,t,py,img)
 	      }
-	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+	png.Encode(os.Stdout, img) 
 }
+func setLine(ymin,ymax,xmin,xmax,width,height float64,contrast,t int64,py int,  img *image.RGBA) {
+	y := getC(ymin,ymax,height,int(py))
+	for px := 0; px < int(width); px++ {
+	    x := getC(xmin,xmax,width,px)
+            z := complex(x, y)
+	    img.Set(px, int(py), newton(z,uint8(contrast),int(t)))
+	}
+}
+func getC(min,max,sizeC float64,coor int) float64{
+	return float64(coor)/sizeC*(max-min) + min
+
+}
+
 func min(x,y uint8) uint8{
 	if x<y {return x}
 	return y
 
 }
-func newton(z complex128, clarity uint8, t int) color.Color {
+func newton(z complex128, contrast uint8, t int) color.Color {
 	const iterations = 200
-	const contrast = 15
-
 	for n := uint8(0); n < iterations; n++ {
 	    z-=f(z,t)
-	    if cmplx.Abs(f(z,t))<0.001 {
-		sf:=min(clarity*n,255)
-		return color.RGBA{sf,sf,0,255}
+	    if cmplx.Abs(f(z,t))<0.01 {
+		sf:=min(contrast*n,255)
+		return color.RGBA{255-sf,255-sf/2,0,255}
 		}
 	}
-    	return color.Black
+    	return color.RGBA{255,255,0,255}
 }
 func f(v complex128,t int) complex128 {
 	w := v
